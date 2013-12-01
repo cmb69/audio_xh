365a7c3099e1e91b2780197d65e65ce62cd01399
cmb69 <cmbecker69@gmx.de>
2013-12-01 20:55:36 +0000

    added Flash fallback

--- index.php
+++ index.php
@@ -2,15 +2,23 @@
 
 function audio($filename)
 {
-    global $pth;
+    global $pth, $cf;
     
-    $url = $pth['folder']['media'] . $filename;
     $displayname = basename($filename);
-    $o = <<<EOS
-<audio src="$url" controls="controls">
-    <a href="$url">$displayname</a>
+    $url = $pth['folder']['media'] . $filename;
+    $player = $pth['folder']['plugins'] . 'audio/emff_stuttgart.swf';
+    $o = <<<HTML
+<audio src="$url" controls="controls" title="$displayname">
+    <object type="application/x-shockwave-flash" data="$player" width="140" height="30">
+        <param name="movie" value="$player"/>
+        <param name="FlashVars" value="src=$url"/>
+        <a href="$url">$displayname</a>
+    </object>
 </audio>
 
-EOS;
+HTML;
+    if (!$cf['xhtml']['endtags']) {
+        $o = str_replace('/>', '>', $o);
+    }
     return $o;
 }
