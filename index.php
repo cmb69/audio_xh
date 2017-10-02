f9b107a58a4b0a8ec1cab93cce7e5b49cc415bfa
Christoph M. Becker <cmbecker69@gmx.de>
2017-10-02 15:56:55 +0200

    Switch to GNU style file header comments

--- index.php
+++ index.php
@@ -1,17 +1,22 @@
 <?php
 
 /**
- * Front-end of Audio_XH.
+ * Copyright 2013-2017 Christoph M. Becker
  *
- * PHP versions 4 and 5
+ * This file is part of Audio_XH.
  *
- * @category  CMSimple_XH
- * @package   Audio
- * @author    Christoph M. Becker <cmbecker69@gmx.de>
- * @copyright 2013-2017 Christoph M. Becker <http://3-magi.net>
- * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
- * @version   SVN: $Id$
- * @link      http://3-magi.net/?CMSimple_XH/Audio_XH
+ * Audio_XH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Audio_XH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Audio_XH.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /*
