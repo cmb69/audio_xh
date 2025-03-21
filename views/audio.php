e870a2f10141de0cf8c7ca25fc46f8ed06fd2542
Christoph M. Becker <cmbecker69@gmx.de>
2025-03-21 16:32:42 +0100

    Re-indent view templates

--- views/audio.php
+++ views/audio.php
@@ -14,7 +14,7 @@ if (!defined("CMSIMPLE_XH_VERSION")) {http_response_code(403); exit;}
 ?>
 
 <audio controls="controls" title="<?=$this->esc($displayname)?>" <?=$this->esc($autoplay)?> <?=$this->esc($loop)?>>
-    <source src="<?=$this->esc($filename)?>.ogg" type="audio/ogg">
-    <source src="<?=$this->esc($filename)?>.mp3" type="audio/mpeg">
-    <a href="<?=$this->esc($filename)?>.mp3"><?=$this->esc($displayname)?></a>
+  <source src="<?=$this->esc($filename)?>.ogg" type="audio/ogg">
+  <source src="<?=$this->esc($filename)?>.mp3" type="audio/mpeg">
+  <a href="<?=$this->esc($filename)?>.mp3"><?=$this->esc($displayname)?></a>
 </audio>
