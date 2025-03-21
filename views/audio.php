216ef71d5640dddae1f030c2d6a8c4766442aef8
Christoph M. Becker <cmbecker69@gmx.de>
2025-03-21 16:31:56 +0100

    Apply static analysis also to view templates

--- views/audio.php
+++ views/audio.php
@@ -1,3 +1,18 @@
+<?php
+
+use Plib\View;
+
+if (!defined("CMSIMPLE_XH_VERSION")) {http_response_code(403); exit;}
+
+/**
+ * @var View $this
+ * @var string $filename
+ * @var string $displayname
+ * @var string $autoplay
+ * @var string $loop
+ */
+?>
+
 <audio controls="controls" title="<?=$this->esc($displayname)?>" <?=$this->esc($autoplay)?> <?=$this->esc($loop)?>>
     <source src="<?=$this->esc($filename)?>.ogg" type="audio/ogg">
     <source src="<?=$this->esc($filename)?>.mp3" type="audio/mpeg">
--- views/info.php
+++ views/info.php
@@ -1,3 +1,16 @@
+<?php
+
+use Plib\View;
+
+if (!defined("CMSIMPLE_XH_VERSION")) {http_response_code(403); exit;}
+
+/**
+ * @var View $this
+ * @var string $version
+ * @var list<object{class:string,message:string}> $checks
+ */
+?>
+
 <h1>Audio <?=$this->esc($version)?></h1>
 <h2><?=$this->text("syscheck_title")?></h2>
 <?foreach ($checks as $check):?>
